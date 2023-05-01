67d19425d006949555d090d5ea494004d4aa04bc
jotaherrera <juanherreramen13@gmail.com>
2023-05-01 11:51:45 -0500

    Add server config

---
+++ src/app.js
@@ -0,0 +1,28 @@
+// Initializations
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const path = require('path');
+
+// Settings
+const app = express();
+const port = process.env.PORT || 3000;
+app.set('routes', path.join(__dirname, 'routes'));
+
+// Statics
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/words'));
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error(err));
+
+// Server is listening
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
